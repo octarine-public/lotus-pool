ef185079dc7639afd0e7e8f3c29441061238b6fc
SicdeX <sicdex@gmail.com>
2024-01-14 20:54:04 +0600

    cleanup

--- src/gui.ts
+++ src/gui.ts
@@ -42,54 +42,29 @@ export class LotusPoolGUI {
 	}
 
 	public Draw(
-		position: Vector3,
+		origin: Vector3,
 		stackCount: number,
 		healthBarOffset: number,
-		additionalSize: number,
+		addSize: number,
 		formatTime: boolean
 	) {
 		if (GameState.UIState !== DOTAGameUIState.DOTA_GAME_UI_DOTA_INGAME) {
 			return
 		}
-
-		const w2s = RendererSDK.WorldToScreen(
-			position.Clone().AddScalarZ(healthBarOffset / 2)
-		)
-
-		if (w2s === undefined || GUIInfo.Contains(w2s)) {
+		const originOffset = origin.Clone().AddScalarZ(healthBarOffset / 2)
+		const w2s = RendererSDK.WorldToScreen(originOffset)
+		if (w2s === undefined || GUIInfo.Contains(w2s) || !this.Update(w2s, addSize)) {
 			return
 		}
 
-		this.updatePositions(w2s, additionalSize)
-
-		if (GUIInfo.Contains(this.position.pos1)) {
-			return
-		}
-
+		const position = this.position
 		const border = GUIInfo.ScaleHeight(4)
-
-		RendererSDK.Image(
-			this.image,
-			this.position.pos1,
-			0,
-			this.position.Size,
-			Color.White
-		)
+		RendererSDK.Image(this.image, position.pos1, 0, position.Size, Color.White)
 
 		// TODO: get max stack count
 		if (stackCount >= 6) {
-			RendererSDK.TextByFlags(
-				stackCount.toString(),
-				this.position,
-				Color.White,
-				2.66
-			)
-			RendererSDK.OutlinedCircle(
-				this.position.pos1,
-				this.position.Size,
-				Color.Green,
-				border
-			)
+			RendererSDK.TextByFlags(stackCount.toString(), position, Color.White, 2.66)
+			RendererSDK.OutlinedCircle(position.pos1, position.Size, Color.Green, border)
 			return
 		}
 
@@ -98,18 +73,12 @@ export class LotusPoolGUI {
 			return
 		}
 
-		RendererSDK.OutlinedCircle(
-			this.position.pos1,
-			this.position.Size,
-			Color.Green,
-			border
-		)
-
+		RendererSDK.OutlinedCircle(position.pos1, position.Size, Color.Green, border)
 		RendererSDK.Arc(
 			-90,
 			Math.max(100 * (remainingTime / this.SpawnTime), 0),
-			this.position.pos1,
-			this.position.Size,
+			position.pos1,
+			position.Size,
 			false,
 			border,
 			Color.Red
@@ -124,7 +93,7 @@ export class LotusPoolGUI {
 			remainingText = remainingTime.toFixed(remainingTime < 2 ? 1 : 0)
 		}
 
-		RendererSDK.TextByFlags(remainingText, this.position, Color.White, 2.66)
+		RendererSDK.TextByFlags(remainingText, position, Color.White, 2.66)
 
 		// draw stack count
 		this.DrawStackCount(stackCount)
@@ -134,23 +103,19 @@ export class LotusPoolGUI {
 		this.sleeper.FullReset()
 	}
 
-	public SentNotification(
-		position: Vector3,
-		statePing: boolean,
-		disableByTime: number
-	) {
+	public SentNotification(origin: Vector3, statePing: boolean, disableByTime: number) {
 		if (this.RemainingTime > 10) {
 			return
 		}
 		const rawTime = GameState.RawGameTime
 		const byRawTime = (rawTime - 95) / 60 <= disableByTime
 		const isDisableByTime = disableByTime === 0 || byRawTime
-		const keyName = position.Length2D + "_sentNotification"
+		const keyName = origin.Length2D + "_sentNotification"
 		if (!statePing || !isDisableByTime || this.sleeper.Sleeping(keyName)) {
 			return
 		}
 		SoundSDK.EmitStartSoundEvent("General.Ping")
-		MinimapSDK.DrawPing(position, Color.White, rawTime + 7)
+		MinimapSDK.DrawPing(origin, Color.White, rawTime + 7)
 		this.sleeper.Sleep(7 * 1000, keyName)
 	}
 
@@ -165,15 +130,14 @@ export class LotusPoolGUI {
 		RendererSDK.TextByFlags(stackCount.toString(), position, Color.White, 2.66)
 	}
 
-	private updatePositions(w2s: Vector2, additionalSize: number) {
+	protected Update(w2s: Vector2, additionalSize: number) {
 		this.baseBoxSize.SetX(GUIInfo.ScaleWidth(this.baseSize + additionalSize))
 		this.baseBoxSize.SetY(GUIInfo.ScaleHeight(this.baseSize + additionalSize))
-
 		const position = w2s.SubtractForThis(
 			this.baseBoxSize.DivideScalar(2).FloorForThis()
 		)
-
 		this.position.pos1.CopyFrom(position)
 		this.position.pos2.CopyFrom(position.Add(this.baseBoxSize))
+		return !GUIInfo.Contains(this.position.pos1)
 	}
 }
